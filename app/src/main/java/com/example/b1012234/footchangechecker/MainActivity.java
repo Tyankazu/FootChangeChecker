94e5db985469db3cf23b2866dd033a25af653734
b1012234 <b1012234@fun.ac.jp>
2015-11-30 21:30:27 +0900

    AddGPS

--- app/src/main/java/com/example/b1012234/footchangechecker/MainActivity.java
+++ app/src/main/java/com/example/b1012234/footchangechecker/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.b1012234.footchangechecker;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
@@ -23,17 +29,25 @@ import java.util.Date;
 import static java.lang.String.*;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LocationListener{
+
+    LocationManager mLocationManager = null;
+    private static final String LOG_TAG ="Location";
 
     private SimpleDateFormat sdf;
     BufferedWriter bw;
     Date date;
     private String path;
+    private String lati;
+    private String longi;
+    private String sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocationManager =(LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
 
         // クリックイベントを取得したいボタン
         Button Start_bt = (Button) findViewById(R.id.btnStart);
@@ -80,7 +94,7 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "move to brake", Toast.LENGTH_SHORT).show();
 
-                String fileName = sdf.format(date) + "FootChangeCheck" + ".csv";
+                String fileName = sdf.format(date) + "FCC" + ".csv";
                 path = Environment.getExternalStorageDirectory() + "/" + fileName;
                 File file = new File(path);
 
@@ -100,7 +114,14 @@ public class MainActivity extends ActionBarActivity {
                         valueOf(month + 1) + "/" + valueOf(day) + "_" + valueOf(hour) + ":"
                         + valueOf(minute) + ":" + valueOf(second) + ":" + valueOf(ms);
 
-                String ChangeAB = nowtime + "," +"AB"+ "\n";
+
+                String ChangeAB = nowtime + "," +"AB"+","+
+                        lati + "," +
+                        longi + "," +
+                        sp + "\n";
+                System.out.println("pushBA"+lati);
+                System.out.println("pushBA"+longi);
+                System.out.println("pushBA"+sp);
 
                 FileOutputStream fos;
                 try {
@@ -132,7 +153,7 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(getApplicationContext(), "move to Accel", Toast.LENGTH_SHORT).show();
 
 
-                String fileName = sdf.format(date) + "FootChangeCheck" + ".csv";
+                String fileName = sdf.format(date) + "FCC" + ".csv";
                 path = Environment.getExternalStorageDirectory() + "/" + fileName;
                File file = new File(path);
 //
@@ -152,7 +173,13 @@ public class MainActivity extends ActionBarActivity {
                         valueOf(month + 1) + "/" + valueOf(day) + "_" + valueOf(hour) + ":"
                         + valueOf(minute) + ":" + valueOf(second) + ":" + valueOf(ms);
 
-                String ChangeBA = nowtime + "," +"BA"+ "\n";
+                String ChangeBA = nowtime + "," +"BA"+","+
+                        lati + "," +
+                        longi + "," +
+                        sp + "\n";
+                System.out.println("pushBA"+lati);
+                System.out.println("pushBA"+longi);
+                System.out.println("pushBA"+sp);
 
                 FileOutputStream fos;
                 try {
@@ -174,10 +201,69 @@ public class MainActivity extends ActionBarActivity {
                     k.printStackTrace();
                 }
 
-
-
             }
         });
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Location lastLocation = mLocationManager.getLastKnownLocation
+                (LocationManager.GPS_PROVIDER);
+        updateDisplayedInfo(lastLocation);
+        //位置情報更新要求(リスナの登録)
+        mLocationManager.requestLocationUpdates
+                (LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //リスナの解除
+        mLocationManager.removeUpdates(this);
+    }
+
+    public void updateDisplayedInfo(Location location){
+        if (location == null){
+            Log.e(LOG_TAG, "location is null.");
+            return;
+        }
+        //緯度の表示更新
+        TextView lat_value = (TextView)findViewById(R.id.latitude);
+        lat_value.setText(Double.toString(location.getLatitude()));
+        //経度の表示更新
+        TextView lon_value = (TextView)findViewById(R.id.longitude);
+        lon_value.setText(Double.toString(location.getLongitude()));
+        //スピードの表示更新
+        TextView spd_value = (TextView)findViewById(R.id.speed);
+        spd_value.setText(Float.toString(location.getSpeed()));
+        lati = Double.toString(location.getLatitude());
+        longi = Double.toString(location.getLongitude());
+        sp = Float.toString(location.getSpeed());
+    }
+
+    public void onLocationChanged(Location location){
+        updateDisplayedInfo(location);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 
